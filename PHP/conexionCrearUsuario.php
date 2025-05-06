7c0273a3e4d760ac04af4867fcee30da9c016c30
LeslieF03 <163460081+LeslieF03@users.noreply.github.com>
2025-05-05 19:09:20 -0600

    Update conexionCrearUsuario.php
    agregue las nuevas variables que pusiste

--- PHP/conexionCrearUsuario.php
+++ PHP/conexionCrearUsuario.php
@@ -3,13 +3,16 @@
 include("conexion.php");
 
 //Variables para recibir y guardar datos del formulario de registro
-$nombres = $_POST("Nombres");
-$P_apellido = $_POST("Primer_Apellido");
-$S_apellido = $_POST("Segundo_Apellido");
-$usuario = $_POST("usuario");
-$contraseña = $_POST("contraseña");
+$nombres = $_POST("nombre");
+$PrimerApellido = $_POST("apellidoP");
+$SegundoApellido = $_POST("apellidom");
+$genero = $_POST("genero");
+$fecha = $_POST("fecha");
+$curp = $_POST("curp");
 $correo = $_POST("correo");
 $telefono = $_POST("telefono");
+$telefono2 = $_POST("telefono2");
+$contraseña = $_POST("contraseña");
 
 $passwordHash = password_hash($contraseña, PASSWORD_BCRYPT); //Para encriptar la contraseña
 $fotoPerfil = "img/$nombres/Avatar.webp";
